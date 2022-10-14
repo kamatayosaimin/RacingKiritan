d6545f1bf65d33222976170f089404095f29e9f6
kamatayosaimin <haneishi4622@gmail.com>
2022-10-15 02:29:39 +0900

    InputSystem設定の修正、各種Scene用Script追加

---
+++ Assets/Scripts/DRun/DRunScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DRunScene : SceneBehaviour
+{
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //}
+
+    protected override void StartChild()
+    {
+    }
+
+    protected override void UpdateChild()
+    {
+    }
+}
