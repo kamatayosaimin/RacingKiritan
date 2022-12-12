071374621de48acd9e482408e3cb8ef60b5d6867
kamatayosaimin <haneishi4622@gmail.com>
2022-12-13 02:14:59 +0900

    GameOverSceneクラスの変数作成

--- Assets/Scripts/GameOver/GameOverScene.cs
+++ Assets/Scripts/GameOver/GameOverScene.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverScene : SceneBehaviour
 {
+    [SerializeField] private string _nextSceneName;
+    [SerializeField] private string _kiritanName;
+    [SerializeField] private string _manhuntName;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] private TextMeshProUGUI _messageText;
+    [SerializeField] private AudioSource _voiceSound;
+    [SerializeField] private GameOverMessageData[] _messageDatas;
 
     //// Start is called before the first frame update
     //void Start()
